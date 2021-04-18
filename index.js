07b5117a638022d9f4d31ae0780c88f85e608ae4
Piotr Domian <domianpiotr111@gmail.com>
2021-04-18 19:33:51 +0200

    Task is done

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-function iterateAndSum() {
-  // Zaimplementuj zadanie zgodnie z wytycznymi
+function iterateAndSum(arr) 
+{
+  a = 0;
+  for(i = 0; i <= arr.length - 1; i++)
+  {
+    a += arr[i];
+  }
+  return a;
 };
 
 module.exports = iterateAndSum;
